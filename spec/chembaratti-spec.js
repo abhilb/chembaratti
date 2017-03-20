328da4dd586ae30ed76d5079fa961d6b8a5d3f9d
abhilb <abhilashbabuj@gmail.com>
2017-03-20 22:56:49 +0100

    First commit

---
+++ spec/chembaratti-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import Chembaratti from '../lib/chembaratti';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Chembaratti', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('chembaratti');
+  });
+
+  describe('when the chembaratti:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.chembaratti')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'chembaratti:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.chembaratti')).toExist();
+
+        let chembarattiElement = workspaceElement.querySelector('.chembaratti');
+        expect(chembarattiElement).toExist();
+
+        let chembarattiPanel = atom.workspace.panelForItem(chembarattiElement);
+        expect(chembarattiPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'chembaratti:toggle');
+        expect(chembarattiPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.chembaratti')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'chembaratti:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let chembarattiElement = workspaceElement.querySelector('.chembaratti');
+        expect(chembarattiElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'chembaratti:toggle');
+        expect(chembarattiElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/chembaratti-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ChembarattiView from '../lib/chembaratti-view';
+
+describe('ChembarattiView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
